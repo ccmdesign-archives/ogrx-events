7f3cd829512903725bab6375138251ff3b411d81
mocxd <mark.adkins.hastings@gmail.com>
2017-04-17 11:17:17 -0400

    add script to detect landscape/portrait images

---
+++ source/js/img-aspect.js
@@ -0,0 +1,28 @@
+// find aspect ratio of images and apply classes
+
+$(function () {
+    // options
+    var cssString = 'img';  // css search string used to select desired images
+    var threshold = 0;  // additional +/- to the aspect ratio (normally 1) required before switching from landscape to portrait
+
+    // check the aspect ratio of each image to see whether to use contain or cover for background-size
+    $(cssString).each(function() {
+        var $e = $(this);
+        var src = $(this).attr('src');
+        var img = new Image();
+
+        // handler to apply class when image fully loaded
+        $(img).on('load', function(){
+            var ar = img.width / img.height;
+            if (ar > 1 + threshold) {
+                $e.addClass( 'landscape' );
+            } else {
+                $e.addClass( 'portrait' );
+            }
+        });
+
+        // set image src after handler is set up, otherwise handler never gets called
+        img.src = src;
+    });
+
+});
